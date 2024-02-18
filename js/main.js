c146e96c6c30428a6b64dd8bf14b797fe297288d
Krasuszz <krasus@hdu.edu.cn>
2024-02-18 20:56:35 +0800

    login test
    login test

--- js/main.js
+++ js/main.js
@@ -1,28 +1,16 @@
+function login()
+{
+    window.location.href = "login.html";
+}
 
 $(document).ready(function (){
-
-    var QRLoginObj = QRLogin({
-        id:"login_container",
-        goto: "https://passport.feishu.cn/suite/passport/oauth/authorize?client_id=cli_a5404aed62f8500b&redirect_uri=https://hdugamemaker.github.io/&response_type=code&state=s0",
-        width: "500",
-        height: "500",
-        style: "width:500px;height:600px"//可选的，二维码html标签的style属性
-    });
-
-    var handleMessage = function (event) {
-        // 使用 matchOrigin 和 matchData 方法来判断 message 和来自的页面 url 是否合法
-        alert("origin: " + event.origin)
-        if(QRLoginObj.matchOrigin(event.origin) && QRLoginObj.matchData(event.data)) {
-            var loginTmpCode = event.data.tmp_code;
-            alert("loginTmpCode: " + loginTmpCode)
-            // 在授权页面地址上拼接上参数 tmp_code，并跳转
-            window.location.href = `https://passport.feishu.cn/suite/passport/oauth/authorize?client_id=cli_a5404aed62f8500b&redirect_uri=https://hdugamemaker.github.io/&response_type=code&state=s0&tmp_code=${loginTmpCode}`;
-        }
-    };
-    if (typeof window.addEventListener != 'undefined') {
-        window.addEventListener('message', handleMessage, false);}
-    else if (typeof window.attachEvent != 'undefined') {
-        window.attachEvent('onmessage', handleMessage);
+    let params = new URLSearchParams(window.location.search);
+    let tmp_code = params.get('tmp_code');
+    if (tmp_code === null) {
+        // 显示错误消息或者重定向用户
+        console.error('未能获取到 logintmpcode');
     }
-
+    else {
+        console.log('logintmpcode: ' + tmp_code);
+    }
 });
