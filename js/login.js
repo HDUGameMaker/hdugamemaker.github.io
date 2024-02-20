e1be330f475e888a82ed1f958b5b67f66049f0eb
Krasuszz <krasus@hdu.edu.cn>
2024-02-20 15:51:54 +0800

    login
    login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,9 @@
+//https://open.feishu.cn/document/common-capabilities/sso/web-application-sso/qr-sdk-documentation
 $(document).ready(function (){
 
     var QRLoginObj = QRLogin({
         id:"login_container",
-        goto: "https://passport.feishu.cn/suite/passport/oauth/authorize?client_id=cli_a5404aed62f8500b&redirect_uri=https://hdugamemaker.github.io/&response_type=code&state=s0",
+        goto: "https://passport.feishu.cn/suite/passport/oauth/authorize?client_id=cli_a558bd2400ba900e&redirect_uri=https://hdugamemaker.github.io/&response_type=code&state=s0",
         width: "256px",
         height: "256px",
         style: "width:100%;"//可选的，二维码html标签的style属性
@@ -10,12 +11,12 @@ $(document).ready(function (){
 
     var handleMessage = function (event) {
         // 使用 matchOrigin 和 matchData 方法来判断 message 和来自的页面 url 是否合法
-        alert("origin: " + event.origin)
+        //alert("origin: " + event.origin)
         if(QRLoginObj.matchOrigin(event.origin) && QRLoginObj.matchData(event.data)) {
             var loginTmpCode = event.data.tmp_code;
-            alert("loginTmpCode: " + loginTmpCode)
+            // alert("loginTmpCode: " + loginTmpCode)
             // 在授权页面地址上拼接上参数 tmp_code，并跳转
-            window.location.href = `https://passport.feishu.cn/suite/passport/oauth/authorize?client_id=cli_a5404aed62f8500b&redirect_uri=https://hdugamemaker.github.io/&response_type=code&state=s0&tmp_code=${loginTmpCode}`;
+            window.location.href = `https://passport.feishu.cn/suite/passport/oauth/authorize?client_id=cli_a558bd2400ba900e&redirect_uri=https://hdugamemaker.github.io/&response_type=code&state=s0&tmp_code=${loginTmpCode}`;
         }
     };
     if (typeof window.addEventListener != 'undefined') {
--- js/main.js
+++ js/main.js
@@ -2,15 +2,65 @@ function login()
 {
     window.location.href = "login.html";
 }
-
-$(document).ready(function (){
+class user
+{
+    tmp_code = '';
+    user_name = '';
+    constructor(tmp_code) {
+        this.tmp_code = tmp_code;
+    }
+    //https://open.feishu.cn/document/server-docs/authentication-management/login-state-management/get
+    get_info()
+    {
+        const that = this;
+        let xhr = new XMLHttpRequest();
+        return new Promise((resolve, reject) =>
+        {
+            xhr.open("post", `https://open.feishu.cn/open-apis/authen/v1/user_info`, true)
+            xhr.setRequestHeader("Authorization", `Bearer ${this.tmp_code}`);
+            xhr.onreadystatechange = function ()
+            {
+                if (xhr.readyState === 4)
+                    if (xhr.status === 200)
+                    {
+                        let result = JSON.parse(xhr.responseText);
+                        that.user_name = result.data.name;
+                        console.log("Get UserName Success");
+                        return resolve();
+                    }
+                else
+                    {
+                        alert("Get UserName failed");
+                        return reject();
+                    }
+            }
+            xhr.send();
+        });
+    }
+}
+$(document).ready(function ()
+{
     let params = new URLSearchParams(window.location.search);
-    let tmp_code = params.get('tmp_code');
-    if (tmp_code === null) {
+    let tmp_code = params.get('code');
+    if (tmp_code === null)
+    {
         // 显示错误消息或者重定向用户
         console.error('未能获取到 logintmpcode');
     }
-    else {
+    else
+    {
         console.log('logintmpcode: ' + tmp_code);
+        let login_div = $('.login');
+        window.user = new user(tmp_code);
+        window.user.get_info()
+            .then(() =>
+            {
+                login_div.empty();
+                login_div.append(`<h1>欢迎！${window.user.user_name}</h1>`);
+            })
+            .then(() =>
+            {
+
+            });
     }
 });
